7f703b8205822bc41e3367bb8f1fb6e134f82646
Pery Lemke <pery.lemke@gmail.com>
2017-12-28 17:21:54 -0200

    Frist commit - Baby Steps always :)

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# thorin
+**Thorin**
+===================
+Thorin is a simple python script to do a snapshots on [Digital Ocean](https://www.digitalocean.com).
+
+Why? I'm study Python and I'm a Lazy SysAdmin.
+
+### ToDo's
+
+* Writing tests.
+* Improve the code
+* Retire api_key and droplet_id in the code.
+
+### Libs
+
+* sys
+* requests
+* json
+* datetime
+
+### How to use
+
+* Clone the repo and access Thorin folder:
+```bash
+$ git clone https://github.com/perylemke/thorin.git
+$ cd thorin
+```
+
+* Open a thorin.py and input a Digital Ocean API Key and Droplet ID
+```bash
+$ vim thorin.py
+>> api_key = 'YOUR KEY HERE!'
+>> droplet_id = 'YOUR DROPLET ID HERE!'
+```
+
+* Execute thorin.py
+```bash
+./thorin.py
+```
+
+* At the end your snapshot was created.
+
+Feedbacks are welcome and if you have a improvement fork us and pull us! :)
+
+### That's it folks!
---
+++ thorin.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import sys
+import requests
+import json
+import datetime
+
+def create_snapshot():
+    # Digital Ocean variables
+    api_key = ''
+    droplet_id = ''
+    snapshot_name = 'THORIN_' + datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    # API URL
+    api_create_snapshot = 'https://api.digitalocean.com/v2/droplets/' + str(droplet_id) + '/actions'
+
+    headers = {
+        'content-type': 'application/json',
+         'Authorization': 'Bearer ' + api_key
+    }
+
+    body = {
+        'type': 'snapshot',
+        'name': snapshot_name
+    }
+
+    print("Realize Live Snapshot...")
+    response = str(requests.post(api_create_snapshot, data=json.dumps(body), headers=headers))
+
+    if response == "<Response [201]>":
+        print("Success - Droplet Created!")
+    elif response == "<Response [401]>":
+        print("Error - Connection refused!")
+    else:
+        print(response)
+
+def main():
+    try:
+        create_snapshot()
+    except(Exception):
+        print('Error!')
+        sys.exit(133)
+
+if __name__ == '__main__':
+    main()
